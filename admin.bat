8ec2c5d498db9a6584f5a8996353a9fc0ea867a1
mirackayademir <kayademurmirac599@gmail.com>
2026-09-10 18:26:43 +0300

    Sync Godot import and UID metadata

--- admin.bat
+++ admin.bat
@@ -1,16 +1,14 @@
 @echo off
+cd /d "%~dp0"
 
-:: Yönetici yetkisi kontrolü
+:: Yonetici yetkisi kontrolu
 net session >nul 2>&1
 if %errorlevel% neq 0 (
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    powershell.exe -NoProfile -Command "Start-Process '%~f0' -Verb RunAs"
     exit /b
 )
 
-:: Aynı klasörde yönetici CMD aç
-start "" cmd.exe /K "cd /d %~dp0"
+:: Proje klasorunde tek PowerShell ac
+powershell.exe -NoExit -Command "Set-Location -LiteralPath '%~dp0'"
 
-:: Aynı klasörde yönetici PowerShell aç
-start "" powershell.exe -NoExit -Command "Set-Location -LiteralPath '%~dp0'"
-
-exit
+exit /b
